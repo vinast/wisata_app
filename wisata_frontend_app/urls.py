8ceab8682d7159812a79dc240b4d30b50fe0add0
vinast <dvvinas2@gmail.com>
2025-03-11 23:25:51 +0700

    push frontend admin

--- wisata_frontend_app/urls.py
+++ wisata_frontend_app/urls.py
@@ -7,5 +7,9 @@ app_name = 'app'
 
 urlpatterns = [
     path('', home_frontend.HomeViews.as_view(), name='index_user'),
+  
+    path('tentang/', include([
+        path('', tentang_frontend.TentangViews.as_view(), name='tentang_user'),
+    ])), 
     
 ]
--- wisata_frontend_app/views/__init__.py
+++ wisata_frontend_app/views/__init__.py
@@ -1,1 +1,2 @@
 from .home_frontend import *
+from .tentang_frontend import *
